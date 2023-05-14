872626a2e710303b2b10055ae326f1bb8ba3e774
ChuprinaVlad <Chuprinavlad@ukr.net>
2023-05-14 21:17:02 +0300

    Initial commit

--- src/main/java/com/example/ua/HelloController.java
+++ src/main/java/com/example/ua/HelloController.java
@@ -1,14 +1,37 @@
 package com.example.ua;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AuthSignInButton;
+
+    @FXML
+    private Button LoginSignUpButton;
+
+    @FXML
+    private TextField Login_field;
+
+    @FXML
+    private PasswordField Password_field;
+
+
+    @FXML
+    void initialize() {
+        AuthSignInButton.setOnAction( event -> {
+            System.out.println("Кнопка для входу!!!");
+        });
     }
 }
